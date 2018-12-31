c6140aced54ae8a4920da9c962a15166abf36bf6
al-blockmed <42915040+al-blockmed@users.noreply.github.com>
2018-12-30 19:48:17 -0800

    Update truffle config for production deployment

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,4 +1,4 @@
-var Migrations = artifacts.require("TruffleStandardMigrations");
+var Migrations = artifacts.require("Migrations");
 
 module.exports = function(deployer, network) {
   if(network == 'development') {
