aa4d858fad9b141f0f47e37e7977489896f30322
al-blockmed <42915040+al-blockmed@users.noreply.github.com>
2018-12-30 19:45:33 -0800

    Update truffle config for production deployment

--- truffle.js
+++ truffle.js
@@ -4,34 +4,32 @@ module.exports = {
   // contracts_build_directory: "./build/contracts",
   networks: {
     development: {
-      // Either use provider here, or specify host and port exclusively
-      // provider: function() {
-      //   var doesntmatter = require('./src/js/web3.min.js');
-      //   if (typeof web3 !== 'undefined') {
-      //       web3 = new Web3(web3.currentProvider);
-      //   } else {
-      //       // set the provider you want from Web3.providers
-      //       web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:7545"));
-      //   }
-      // },
-      host: "127.0.0.1",
+      host: "ganache",
       port: 7545,
       network_id: "*", // Match any network id
       gas: 4700000
     },
-    rinkeby: {
-      host: "127.0.0.1", // Connect to geth on the specified
-      port: 8545,
-      from: "", // default address to use for any transaction Truffle makes during migrations
-      network_id: 15,
-      gas: 6000000 // Gas limit used for deploys
+    ropstan: {
+      provider: function() {
+        return new HDWalletProvider(process.env.MNENOMIC, "https://ropsten.infura.io/v3/" + process.env.INFURA_API_KEY, 1);
+      },
+      network_id: 3,
+      gas: 4000000, // Gas limit used for deploys
+      gasPrice: 1000000000
     },
-    testnet: {
-      host: "127.0.0.1", // Connect to geth on the specified
-      port: 8545,
-      from: "", // default address to use for any transaction Truffle makes during migrations
-      network_id: 15,
-      gas: 6000000 // Gas limit used for deploys
-    }
+    rinkeby: {
+      provider: function() {
+        return new HDWalletProvider(process.env.MNENOMIC, "https://rinkeby.infura.io/v3/" + process.env.INFURA_API_KEY, 1);
+      },
+      network_id: 4,
+      gas: 4000000, // Gas limit used for deploys
+      gasPrice: 1000000000
+    },
+    mainnet: {
+      provider: function() {
+        return new HDWalletProvider(process.env.MNENOMIC, "https://mainnet.infura.io/v3/" + process.env.INFURA_API_KEY, 0);
+      },
+      network_id: 1,
+    },
   }
 };
