3bd751a2412f4bd991f622a3a77db18bb1ebad9f
al-blockmed <42915040+al-blockmed@users.noreply.github.com>
2018-12-31 22:31:49 -0800

    Add fractional tx fee use case to fund exchanger wallet on each exchange tx

--- contracts/BMV-USD-ExchangeContract.sol
+++ contracts/BMV-USD-ExchangeContract.sol
@@ -21,6 +21,7 @@ contract TradeContract is SafeMath {
     bool public allowIpfsReg = true;
     uint256 public constant decimals = 18;
     uint256 private constant eth_to_wei = 10 ** decimals;
+    uint256 private constant exchange_tx_fee = 1224547000000000; // wei
 
     uint256 public exchangeRate = 100 * eth_to_wei; // e.g. 1 eth = 100.000000000000000000 USD = 100 BMD
 
@@ -92,6 +93,10 @@ contract TradeContract is SafeMath {
         emit NewExchangeRate("New exchange rate set", newExRate);
     }
 
+    function updateWithdrawAddress(address _target_wallet) external restricted_withdraw {
+        target_wallet = _target_wallet;
+    }
+
     function takerBuyAsset() public payable {
         if (allowTokenEx || msg.sender == owner) {
             // We block out the target_wallet, b/c it could drain the tokens without spending any eth
@@ -103,9 +108,14 @@ contract TradeContract is SafeMath {
             // This means, you will need Token balance under THIS CONTRACT!!!!!!!!!!!!!!!!!!!!!!
             require(InterfaceERC20(exchanging_token_addr).transfer(msg.sender, tokens), "Exchanged token transfer failed!");
             emit ExchangeTokens(msg.sender, msg.value, tokens);
+            uint256 subtract_tx_fee = msg.value;
+            if(exchanger != 0) {
+                contributeTx();
+                subtract_tx_fee = safeSub(msg.value, exchange_tx_fee);
+            }
             // Only triggers when the target wallet has been configured, otherwise, leave the Eth in this contract
             if(target_wallet != 0) {
-                withdraw(msg.value); // requesting another 2300 gas, revert leads to token sale failure
+                withdraw(subtract_tx_fee); // requesting another 2300 gas, revert leads to token sale failure
             }
         }
         else
@@ -125,6 +135,14 @@ contract TradeContract is SafeMath {
             InterfaceERC20(exchanging_token_addr).transfer(owner, remain_balance), 
             "Withdraw tokens back to owner failed before self desctruction!");
         selfdestruct(target_wallet);
+    }
+
+    // Each tx will contribute a little tx fee to the exchanger address to automate
+    // exchange rate update. This funds the wallet to do so.
+    function contributeTx() private {
+        require(exchanger != 0, "Exchanger wallet not set, can't fund it!");
+        exchanger.transfer(exchange_tx_fee);
+        emit Withdrawal(msg.sender, exchanger, exchange_tx_fee);
     }
 
     function withdraw(uint256 amount) private {
