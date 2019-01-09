f11a221d1a2fdf9724e7fd84769c5ac42dfd38dc
al-blockmed <42915040+al-blockmed@users.noreply.github.com>
2019-01-08 20:49:25 -0800

    Fix HDWalletProvider not found error

--- truffle.js
+++ truffle.js
@@ -1,3 +1,5 @@
+var HDWalletProvider = require("truffle-hdwallet-provider");
+
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // for more about customizing your Truffle configuration!
